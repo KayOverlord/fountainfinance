29487f3b7b1b84d489b20dc056280255ef40a9fb
Kabelo Leboa <kabelo@makebetter.co.za>
2022-02-08 01:21:11 +0200

    Layout update

---
+++ styles/Theme.tsx
@@ -0,0 +1,68 @@
+import { createTheme } from '@mui/material/styles';
+
+  export const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#1565c0',
+      },
+      secondary: {
+        main: '#2ec5d3',
+      },
+      background: {
+        default: '#192231',
+        paper: '#24344d',
+      },
+      text:{
+        primary:"#fff",
+        secondary:"rgba(255, 255, 255, 0.7)",
+        disabled:"rgba(255, 255, 255, 0.5)"
+      },
+      
+    }
+  //   props: {
+  //     MuiButton: {
+  //       size: 'small',
+  //     },
+  //     MuiButtonGroup: {
+  //       size: 'small',
+  //     },
+  //     MuiCheckbox: {
+  //       size: 'small',
+  //     },
+  //     MuiFab: {
+  //       size: 'small',
+  //     },
+  //     MuiFormControl: {
+  //       margin: 'dense',
+  //       size: 'small',
+  //     },
+  //     MuiFormHelperText: {
+  //       margin: 'dense',
+  //     },
+  //     MuiIconButton: {
+  //       size: 'small',
+  //     },
+  //     MuiInputBase: {
+  //       margin: 'dense',
+  //     },
+  //     MuiInputLabel: {
+  //       margin: 'dense',
+  //     },
+  //     MuiRadio: {
+  //       size: 'small',
+  //     },
+  //     MuiSwitch: {
+  //       size: 'small',
+  //     },
+  //     MuiTextField: {
+  //       margin: 'dense',
+  //       size: 'small',
+  //     },
+  //     MuiTooltip: {
+  //       arrow: true,
+  //     },
+  //   },
+  // };
+    
+  });
+
