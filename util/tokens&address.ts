036ca80e27bad5880d4abe25294d7847fefc4eaf
Kabelo Leboa <kavinleboa@gmail.com>
2022-07-14 12:02:12 +0200

    points update

--- util/tokens&address.ts
+++ util/tokens&address.ts
@@ -11,7 +11,9 @@ export const LP_Tokens =[{
     Fountain_address:"0x0ab865137ddb23e99447369A0340C7eB92Ad1C0F",
     symbol:"FTN-COMBO",
     image:"https://polygonscan.com/token/images/furucombo_32.png",
-    Abi:ComboAbi
+    Abi:ComboAbi,
+    points:19
+
 },
 {
     title:"BNB",
@@ -19,7 +21,8 @@ export const LP_Tokens =[{
     Fountain_address:"0x7B929E2A8053d7B35DE95068e7a099CE4D02d96a",
     symbol:"FTN-BNB",
     image:"https://polygonscan.com/token/images/bnb_28_2.png",
-    Abi:BnBAbi
+    Abi:BnBAbi,
+    points:16
 },
 {
     title:"WETH",
@@ -27,14 +30,16 @@ export const LP_Tokens =[{
     Fountain_address:"0x77B9F288053685D0876387d9b4D8C1A3e2Aa6018",
     symbol:"FTN-WETH",
     image:"https://polygonscan.com/token/images/wETH_32.png",
-    Abi:WethAbi
+    Abi:WethAbi,
+    points:16
 },{
     title:"WMATIC",
     address:"0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270",
     Fountain_address:"0x9EEB47fa2D9b24f136D8BA4e0303Ca95A288481A",
     symbol:"FTN-WMATIC",
     image:"https://polygonscan.com/token/images/wMatic_32.png",
-    Abi:WMaticAbi
+    Abi:WMaticAbi,
+    points:18
 },
 {
     title:"GOLI",
@@ -42,7 +47,8 @@ export const LP_Tokens =[{
     Fountain_address:"0x2E39D2AE5b500641D819fF15d2F0141987eDBfDf",
     symbol:"FTN-GOLI",
     image:"https://polygonscan.com/token/images/golinenetwork_32.png",
-    Abi:GoliAbi
+    Abi:GoliAbi,
+    points:16
 },
 {
     title:"SAND",
@@ -50,7 +56,8 @@ export const LP_Tokens =[{
     Fountain_address:"0x7A0862e1170E38dd912151eBfdFF8F9242F61D08",
     symbol:"FTN-SAND",
     image:"https://polygonscan.com/token/images/sand_32.png",
-    Abi:SANDAbi 
+    Abi:SANDAbi,
+    points:18
 },
 
 ]
@@ -69,13 +76,5 @@ FountainFactory:"0xDE7DBC03c90b0C6029F435865Cd92212D0e0cAc3"
  * bnb
  * weth
  * 0x271d70ED8a1ab6890aFaD1D99e64450972D96a18
- * 
- * 
- * Angels
- * 0xD9Ec1613C9E9C41c91C76C46b342B2b8690ba062
- * 0x08Ea69459ccBc465A1aF1F7462173bA35264b149
- * 0x062FfE63b7A0d7f27A8105e717c6Ea45E5848AD3
- * I have a problem In regards to the Trevi system, so after approving and depositing the lp Tokens to the fountain and calling joinAngel(angel address) the execution gets reverted
-What am I missing?
  * 
  */
